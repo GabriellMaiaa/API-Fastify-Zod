e783c34248921f3d07f0ad565bcd11bd9f9c596d
GabriellMaiaa <gribeiromaia736@gmail.com>
2024-12-05 11:44:58 -0400

    #04 - Adding our test enviroment to application

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,16 @@
 // Vamos usar a biblioteca ZOD que funciona na parte da validação de dados.
 // Vamos usar primeiramente para validar nossas variáveis de ambiente
 
-import "dotenv/config";
+import { config } from "dotenv";
 import { z } from "zod";
+
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === "test") {
+  config({ path: ".env.test" });
+} else {
+  config();
+}
 
 const envSchema = z.object({
   // Estamos criando um schema para mostrar como devem retornar os valores de nossas var de ambiente
--- test/transaction.spec.ts
+++ test/transaction.spec.ts
@@ -1,13 +1,21 @@
-import { expect, beforeAll, afterAll, it } from "vitest";
+import { expect, beforeAll, afterAll, it, beforeEach } from "vitest";
 import { app } from "../src/app"; // Vamos importar o nosso app que tem as config dele e não sobe um servidor
 import request from "supertest";
 import { describe } from "node:test";
+import { title } from "node:process";
+import { execSync } from "node:child_process";
 
 // Como nossas ROTAS e PLUGINS são Assíncronos, com operações de bancos e outras edições, precisamos deixar claro para nossos testes que a operação
 // já começou e quando deve ser concluída, para que o nosso valor de APP seja válido, e para isso usamos duas funções:
 
 describe("Transactions Routes", () => {
   // Aqui estou descrevendo o que estou fazendo e criando um CONTEXTO
+
+  beforeEach(async () => {
+    execSync("npx knex migrate:latest");
+    await app.ready();
+  });
+
   beforeAll(async () => {
     //Aqui falo que o nosso App antes de tudo já esteja pronto
     await app.ready();
@@ -27,8 +35,22 @@ describe("Transactions Routes", () => {
         amount: 5000,
         type: "credit",
       })
-      .expect(201); // E no final, SEMPRE devemos ter o expect do nosso teste
+      .expect(201); // E no final, SEMPRE devemos ter o expect do nosso testes
   });
 
-  it("should be able to list all transactions", async () => {});
+  it("should be able to list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "Teste",
+        amount: 700,
+        type: "credit",
+      });
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const listTransactionsResponse = await request(app.server) //Sempre chamo nosso servidor dessa forma com o SERVER do node
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+  });
 });
